28421991d33e643c2dcf6a0cb745c18be130979c
Ruben <appnidee@gmail.com>
2022-02-28 22:01:27 +0100

    rest

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@
      const PORT = 3000
      app.use(express.json())
      app.get("/", (req, res) => {
-       res.send("Hello World Appnidee and the rsest")
+       res.send("Hello World Appnidee and the rest")
      })
      app.listen(PORT, () => {
        console.log(`Server is listening on port ${PORT}`)
